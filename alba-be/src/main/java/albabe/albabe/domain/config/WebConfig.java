eef9796cdd50b2a6e1200a8b3a3702f059fd9937
sea5812 <sea5812@naver.com>
2024-10-10 12:53:30 +0900

    cansignup

--- alba-be/src/main/java/albabe/albabe/domain/config/WebConfig.java
+++ alba-be/src/main/java/albabe/albabe/domain/config/WebConfig.java
@@ -1,6 +1,5 @@
 package albabe.albabe.domain.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
