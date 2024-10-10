cf73a742a4a7addd220e6fb880cdb13952f9572f
sea5812 <sea5812@naver.com>
2024-10-10 13:29:40 +0900

    dd

--- alba-be/src/main/java/albabe/albabe/domain/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package albabe.albabe.domain.repository;
-
-import albabe.albabe.domain.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByUsername(String username);
-}
