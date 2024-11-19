43773b87707ae681fab335017cf134e240c949c1
seongch7 <152271513+seongch7@users.noreply.github.com>
2024-11-19 23:57:32 +0900

    feat : applicant_count 증가 오류 정정

--- src/main/java/albabe/albabe/domain/service/JobApplicationService.java
+++ src/main/java/albabe/albabe/domain/service/JobApplicationService.java
@@ -43,7 +43,6 @@ public class JobApplicationService {
 
         jobApplicationRepository.save(application);
 
-        jobPost.incrementApplicantCount();
         jobPostRepository.save(jobPost);
     }
 
